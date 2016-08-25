d5ffa6413bb0589bdf6eeca9002f7fae1c915fd1
xieyuliang <xieyuliang1985@126.com>
2016-08-25 10:03:03 +0800

    CommitBasicFiles
    CommitBasicFiles

---
+++ DTUServiceMonitor/DTUServiceMonitor/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace DTUServiceMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtServerPort = new System.Windows.Forms.TextBox();
+            this.txtDTUPort = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(386, 21);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(85, 48);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "启动";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtServerPort
+            // 
+            this.txtServerPort.Location = new System.Drawing.Point(207, 21);
+            this.txtServerPort.Name = "txtServerPort";
+            this.txtServerPort.Size = new System.Drawing.Size(100, 21);
+            this.txtServerPort.TabIndex = 1;
+            // 
+            // txtDTUPort
+            // 
+            this.txtDTUPort.Location = new System.Drawing.Point(207, 48);
+            this.txtDTUPort.Name = "txtDTUPort";
+            this.txtDTUPort.Size = new System.Drawing.Size(100, 21);
+            this.txtDTUPort.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(151, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "服务端口";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(133, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "DTU服务端口";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(505, 24);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(85, 48);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 77);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(808, 479);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 558);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDTUPort);
+            this.Controls.Add(this.txtServerPort);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtServerPort;
+        private System.Windows.Forms.TextBox txtDTUPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ DTUServiceMonitor/DTUServiceMonitor/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DTUServiceMonitor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Queue<string> loggerQueue = new Queue<string>();
+        private List<Socket> clientPool = new List<Socket>(2);
+        private volatile bool IsRunning = true;
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (DTUWrapper.DSStartService(ushort.Parse(txtDTUPort.Text)) != 0)
+                {
+
+                    loggerQueue.Enqueue("启动DTU服务成功");
+                }
+                else
+                {
+                    loggerQueue.Enqueue("启动DTU服务失败");
+                }
+
+                IPEndPoint serverIP = new IPEndPoint(IPAddress.Any, int.Parse(txtServerPort.Text)); //本机预使用的IP和端口
+                Socket skServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                skServer.Bind(serverIP);
+                skServer.Listen(100);
+
+                var mainThread = new Thread(() =>
+                {
+                    try
+                    {
+                        while (IsRunning)
+                        {
+                            Socket clientSocket;
+                            try
+                            {
+                                //当有可用的客户端连接尝试时执行，并返回一个新的socket,用于与客户端之间的通信
+                                clientSocket = skServer.Accept();
+                                clientPool.Add(clientSocket);
+                                var execThread = new Thread(o =>
+                                {
+                                    var sock = o as Socket;
+                                    var readEvent = new SocketAsyncEventArgs();
+                                    readEvent.Completed += (readSock, eventArgs) =>
+                                    {
+                                        var currentsock = readSock as Socket;
+                                        if (eventArgs.BytesTransferred > 0 &&
+                                            eventArgs.SocketError == SocketError.Success)
+                                        {
+                                            //切换数据
+
+                                            //发送数据给currentsock
+                                        }
+                                        else
+                                        {
+                                            currentsock.Close();
+                                        }
+                                    };
+                                    sock.ReceiveAsync(readEvent);
+
+                                    var sendEvent = new SocketAsyncEventArgs();
+                                    sendEvent.Completed += (sendSock, eventArgs) =>
+                                    {
+                                        var currentsock = sendSock as Socket;
+                                        if (eventArgs.BytesTransferred > 0 &&
+                                            eventArgs.SocketError == SocketError.Success)
+                                        {
+                                            //切换数据
+
+                                            //查找到对应的DTU
+
+                                            //发送数据给DTU
+                                        }
+                                        else
+                                        {
+                                            currentsock.Close();
+                                        }
+                                    };
+                                    sock.SendAsync(sendEvent);
+                                });
+                                execThread.Start(clientSocket);
+                            }
+                            catch (Exception ex)
+                            {
+                                loggerQueue.Enqueue("接收用户连接失败" + ex.Message);
+                                continue;
+                            }
+                            Thread.Sleep(10);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        loggerQueue.Enqueue("主线程内发生错误" + ex.Message + ",主线程已经退出!");
+                    }
+
+                });
+                mainThread.Start();
+
+            }
+            catch (Exception ex)
+            {
+                loggerQueue.Enqueue("程序界面线程出错，" + ex.Message + ",程序界面线程已经退出!"); 
+            }
+        }
+
+
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            IsRunning = false;
+            foreach (var client in clientPool)
+            {
+                client.Close(1);
+            }
+            DTUWrapper.DSStopService();
+        }
+    }
+}
